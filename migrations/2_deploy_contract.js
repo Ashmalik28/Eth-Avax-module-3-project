8c621865051f3deb22325abbc40daa8a9a502ddd
Ashyam Malik <ashyammalik052@gmail.com>
2023-07-19 10:20:38 +0000

    Initial commit: Adding contract files

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,8 +1,12 @@
-const Assessment = artifacts.require("Myproject");
+const Ashyam = artifacts.require("Ashyam");
 
 module.exports = function(deployer) {
-  deployer.deploy(Assessment, 0).then(async () => {
-    const deployedContract = await Assessment.deployed();
+  const name = "Ashyam";
+  const symbol = "Ash";
+  const decimalsValue = 18;
+
+  deployer.deploy(Ashyam, name, symbol, decimalsValue).then(async () => {
+    const deployedContract = await Ashyam.deployed();
     console.log("Contract deployed at:", deployedContract.address);
   });
 };
